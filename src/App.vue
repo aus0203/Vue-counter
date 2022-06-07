72da74743257c9b9085f24b19e5781a8daed0d57
MERVE <merve@MERVEs-MacBook-Pro.local>
2022-06-07 14:45:04 +0200

    Vue counter created

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,64 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>{{ title }}</h1>
+  <div class="counter">
+    <CounutingButton symbol="-" @click="decrement"/>
+    <h3 class="value">{{ value }}</h3>
+    <CounutingButton symbol="+" @click="increment"/>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import CounutingButton from "./components/y-button.vue";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  name: "App",
+  components: { CounutingButton },
+  data() {
+    return {
+      title: "Vue Counter practice",
+      value: 0,
+    };
+  },
+  methods:{
+    decrement(){
+      this.value--;
+    },
+    increment(){
+      this.value++;
+    }
   }
-}
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+body {
+  background: #f9f9f9;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: "Nunito Sans", sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin: 64px;
+}
+
+.counter {
+  display: flex;
+  gap: 52px;
+  margin: 80px;
+  justify-content: center;
+  align-items: center;
+}
+
+.value{
+    transition: .3s ease-in-out;
 }
 </style>
---
+++ src/components/y-button.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="button">
+    <p class="symbol">{{symbol}}</p>
+  </div>
+</template>
+
+<script>
+
+export default{
+  props:['symbol']
+}
+
+</script>
+
+<style>
+.button {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 48px;
+  height: 48px;
+  border: 1px solid #d2d2d2;
+  border-radius: 50px;
+  cursor: pointer;
+  filter: invert(30%);
+}
+
+.button:hover{
+  background: #eeeeee;
+  border: none;
+  transition: 300ms ease-in-out;
+}
+
+.symbol{
+  font-size: 20px;
+}
+</style>
